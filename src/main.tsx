8adf5cd7239e468d142b200af452b55bca68930a
mailfaruqi <ismailfaruqi7@gmail.com>
2024-07-10 15:12:26 +0700

    feat: setup react router and react map gl

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+import { App } from "./routes/maps";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div>React Mapbox GL</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
