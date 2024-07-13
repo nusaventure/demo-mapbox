28ab0c8a4aa8e989a6a51bd9b264b0ed1f9f7c55
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-13 14:34:08 +0700

    feat: example for multiple markers in a map

--- src/routes/maps.tsx
+++ src/routes/maps.tsx
@@ -5,12 +5,47 @@ import "../mapbox-gl.css";
 const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 export function App() {
-  const coordinate = {
+  return (
+    <div>
+      <MapboxMap
+        coordinate={{
+          latitude: -6.1753924,
+          longitude: 106.8271528,
+          zoom: 14,
+        }}
+      />
+
+      {/* <MapboxMapMarkers
+        map={{
+          latitude: -6.1753924,
+          longitude: 106.8271528,
+          zoom: 14,
+        }}
+        places={[
+          { latitude: -6.1753924, longitude: 106.8271528 },
+          { latitude: -6.1763924, longitude: 106.9271528 },
+          { latitude: -6.1773924, longitude: 106.8371528 },
+        ]}
+      /> */}
+    </div>
+  );
+}
+
+interface MapboxMapProps {
+  coordinate: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  };
+}
+
+export function MapboxMap({
+  coordinate = {
     latitude: -6.1753924,
     longitude: 106.8271528,
     zoom: 14,
-  };
-
+  },
+}: MapboxMapProps) {
   return (
     <Map
       mapboxAccessToken={mapboxAccessToken}
