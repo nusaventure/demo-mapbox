c5794e7cc58d16fea0c631310bef0f00487b7c8c
mailfaruqi <ismailfaruqi7@gmail.com>
2024-07-13 14:12:46 +0700

    feat: setup mapbox

--- src/routes/maps.tsx
+++ src/routes/maps.tsx
@@ -1,16 +1,16 @@
-import Map from "react-map-gl/maplibre";
-import "maplibre-gl/dist/maplibre-gl.css";
+import Map from "react-map-gl";
 
 export function App() {
   return (
     <Map
+      mapboxAccessToken="pk.eyJ1IjoibWFpbGZhcnVxaSIsImEiOiJjbHgwMmw1MWUwY2FtMmpxdWEzZzZuaGxtIn0.y1WITnIkcAoNet2MZEL5jQ"
       initialViewState={{
         longitude: -122.4,
         latitude: 37.8,
         zoom: 14,
       }}
       style={{ width: 600, height: 400 }}
-      mapStyle="https://api.maptiler.com/maps/streets/style.json?key=CUpbwB176UHUnUpFPcsN"
+      mapStyle="mapbox://styles/mapbox/streets-v9"
     />
   );
 }
