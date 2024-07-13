6d5769ec37a8afd22fcbdbc068d29a8170d52c4c
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-13 14:26:35 +0700

    feat: setup mapbox css with marker

--- src/routes/maps.tsx
+++ src/routes/maps.tsx
@@ -1,16 +1,30 @@
-import Map from "react-map-gl";
+import Map, { Marker } from "react-map-gl";
+
+import "../mapbox-gl.css";
+
+const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 export function App() {
+  const coordinate = {
+    latitude: -6.1753924,
+    longitude: 106.8271528,
+    zoom: 14,
+  };
+
   return (
     <Map
-      mapboxAccessToken="pk.eyJ1IjoibWFpbGZhcnVxaSIsImEiOiJjbHgwMmw1MWUwY2FtMmpxdWEzZzZuaGxtIn0.y1WITnIkcAoNet2MZEL5jQ"
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
+      mapboxAccessToken={mapboxAccessToken}
+      initialViewState={coordinate}
       style={{ width: 600, height: 400 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      <Marker
+        longitude={coordinate.longitude}
+        latitude={coordinate.latitude}
+        anchor="bottom"
+      >
+        <img src="/images/pin.png" width={50} height={50} />
+      </Marker>
+    </Map>
   );
 }
